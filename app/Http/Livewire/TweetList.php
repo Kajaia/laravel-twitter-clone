e3368680d64365fea97f22276e7174036813af3e
Kajaia <lashakajaia31@gmail.com>
2022-05-26 23:21:01 +0400

    Refactored livewire TweetList class code

--- app/Http/Livewire/TweetList.php
+++ app/Http/Livewire/TweetList.php
@@ -6,16 +6,13 @@ use App\Models\Favourite;
 use App\Models\Like;
 use App\Models\Reply;
 use App\Models\Tweet;
-use App\Notifications\UserNotification;
-use Illuminate\Support\Facades\Notification;
+use App\Services\TweetService;
 use Livewire\Component;
 
 class TweetList extends Component
 {
     public $content;
-
     public $tweet;
-
     public $perPageReplies = 3;
 
     protected $listeners = [
@@ -27,25 +24,27 @@ class TweetList extends Component
         'content' => 'required|max:280'
     ];
 
-    public function updated($propertyName) {
+    public function updated($propertyName) 
+    {
         $this->validateOnly($propertyName);
     }
 
-    public function deleteTweet() {
-        Tweet::findOrFail($this->tweet->id)
-            ->delete();
+    public function deleteTweet() 
+    {
+        Tweet::findOrFail($this->tweet->id)->delete();
 
         $this->emit('deleteTweet');
     }
 
-    public function deleteReply($replyId) {
-        Reply::findOrFail($replyId)
-            ->delete();
+    public function deleteReply($replyId) 
+    {
+        Reply::findOrFail($replyId)->delete();
 
         $this->emit('deleteReply');
     }
 
-    public function storeReply() {
+    public function storeReply(TweetService $service) 
+    {
         $this->validate();
 
         Reply::create([
@@ -54,52 +53,29 @@ class TweetList extends Component
             'user_id' => auth()->user()->id
         ]);
 
-        if($this->tweet->user_id !== auth()->user()->id) {
-            Notification::send(Tweet::find($this->tweet->id)->user, new UserNotification(auth()->user(), auth()->user()->name.' replied to your tweet.', $this->tweet->id));
+        if(!$service->isAuthor($this->tweet->user->id)) {
+            $service->replyOnTweetNotification($this->tweet);
         }
 
         $this->reset('content');
     }
 
-    public function likeTweet() {
-        if(!in_array(auth()->user()->id, $this->tweet->likes->pluck('user_id')->toArray())) {
-            Like::create([
-                'tweet_id' => $this->tweet->id,
-                'user_id' => auth()->user()->id
-            ]);
-
-            if($this->tweet->user_id !== auth()->user()->id) {
-                Notification::send(Tweet::find($this->tweet->id)->user, new UserNotification(auth()->user(), auth()->user()->name.' liked your tweet.', $this->tweet->id));
-            }
-        } else {
-            Like::where('tweet_id', $this->tweet->id)
-                ->where('user_id', auth()->user()->id)
-                ->delete();
-
-            if($this->tweet->user_id !== auth()->user()->id) {
-                Notification::send(Tweet::find($this->tweet->id)->user, new UserNotification(auth()->user(), auth()->user()->name.' unliked your tweet.', $this->tweet->id));
-            }
-        }
+    public function likeTweet(TweetService $service) 
+    {
+        $service->likeUnlikeTweet($this->tweet);
 
         $this->emit('likeTweet');
     }
 
-    public function addToFavourites() {
-        if(!in_array(auth()->user()->id, $this->tweet->favourites->pluck('user_id')->toArray())) {
-            Favourite::create([
-                'tweet_id' => $this->tweet->id,
-                'user_id' => auth()->user()->id
-            ]);
-        } else {
-            Favourite::where('tweet_id', $this->tweet->id)
-                ->where('user_id', auth()->user()->id)
-                ->delete();
-        }
+    public function addToFavourites(TweetService $service) 
+    {
+        $service->addFavourites($this->tweet);
 
         $this->emit('addToFavourites');
     }
 
-    public function perPageRepliesIncrease() {
+    public function perPageRepliesIncrease() 
+    {
         $this->perPageReplies += 3;
     }
 
@@ -111,8 +87,7 @@ class TweetList extends Component
             'replies' => Reply::where('tweet_id', $this->tweet->id)
                 ->orderBy('created_at', 'desc')
                 ->cursorPaginate($this->perPageReplies),
-            'repliesCount' => Reply::where('tweet_id', $this->tweet->id)
-                ->count()
+            'repliesCount' => Reply::where('tweet_id', $this->tweet->id)->count()
         ]);
     }
 }
--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -3,10 +3,13 @@
 namespace App\Services;
 
 use App\Http\Requests\TweetRequest;
+use App\Models\Favourite;
 use App\Models\Like;
 use App\Models\Reply;
 use App\Models\Tweet;
+use App\Notifications\UserNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class TweetService {
 
@@ -84,6 +87,62 @@ class TweetService {
         }
 
         return response()->json($unlike);
+    }
+
+    public function likeUnlikeTweet(Tweet $tweet)
+    {
+        if(!$this->isLiked(auth()->user()->id, $tweet->id)) {
+            Like::create([
+                'tweet_id' => $tweet->id,
+                'user_id' => auth()->user()->id
+            ]);
+
+            if(!$this->isAuthor($tweet->user_id)) {
+                $this->likeTweetNotification($tweet, 'liked');
+            }
+        } else {
+            Like::where('tweet_id', $tweet->id)
+                ->where('user_id', auth()->user()->id)
+                ->delete();
+
+            if(!$this->isAuthor($tweet->user_id)) {
+                $this->likeTweetNotification($tweet, 'unliked');
+            }
+        }
+    }
+
+    public function isAuthor($userId) 
+    {
+        return $userId === auth()->user()->id;
+    }
+
+    public function replyOnTweetNotification(Tweet $tweet)
+    {
+        Notification::send($tweet->user, new UserNotification(auth()->user(), auth()->user()->name.' replied to your tweet.', $tweet->id));
+    }
+
+    public function likeTweetNotification(Tweet $tweet, $str)
+    {
+        Notification::send($tweet->user, new UserNotification(auth()->user(), auth()->user()->name . ' ' . $str . ' your tweet.', $tweet->id));
+    }
+
+    public function isFavourited(Tweet $tweet)
+    {
+        return in_array(auth()->user()->id, $tweet->favourites->pluck('user_id')->toArray());
+    }
+
+    public function addFavourites(Tweet $tweet)
+    {
+        if(!$this->isFavourited($tweet)) {
+            Favourite::create([
+                'tweet_id' => $tweet->id,
+                'user_id' => auth()->user()->id
+            ]);
+        } else {
+            Favourite::where('tweet_id', $tweet->id)
+                ->where('user_id', auth()->user()->id)
+                ->delete();
+        }
     }
 
 }
