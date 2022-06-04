a665cdab986ce7dc429c0f64bc745a5ffe748946
Lasha Kajaia <lashakajaia31@gmail.com>
2022-06-04 12:22:39 +0400

    Fixed error for specific tweet visibility from guest side

--- resources/views/tweet.blade.php
+++ resources/views/tweet.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-8">
+    <div class="@auth col-md-8 @else col-md-12 @endauth">
         <livewire:tweet-list :tweet="$tweet" />
     </div>
+    @auth
     <x-the-sidebar :user="auth()->user()" />
+    @endauth
 </div>
 @endsection
